555f9d6a6707334fbd5bba1c1ab3bcaa108ee342
Alexis Hernández <hernandez.fuertes.alexis@gmail.com>
2021-10-18 11:31:36 -0600

    Terminada funcioalidad de lector de http y mapa

--- lib/pages/direcciones_page.dart
+++ lib/pages/direcciones_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_scanner/providers/scan_list_provider.dart';
+import 'package:qr_scanner/widgets/scan_tiles.dart';
 
 
 class DireccionesPage extends StatelessWidget {
@@ -8,19 +9,6 @@ class DireccionesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final scanListProvider = Provider.of<ScanListProvider>(context, listen:true);
-    final scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: ( _ , i) => ListTile(
-        leading: Icon(Icons.home_rounded, color: Theme.of(context).primaryColor),
-        title :  Text(scans[i]!.valor.toString()),
-        subtitle : Text(scans[i]!.id.toString()),
-        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-        
-      )
-      );
+    return const ScanTiles(tipo:'http');
   }
 }
--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:qr_scanner/providers/scan_list_provider.dart';
+import 'package:qr_scanner/widgets/scan_tiles.dart';
 
 
 class MapasPage extends StatelessWidget {
@@ -8,27 +7,6 @@ class MapasPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context, listen:true);
-    final scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: ( _ , i) => Dismissible(
-        key: UniqueKey(),
-        onDismissed: (DismissDirection direction){
-          Provider.of<ScanListProvider>(context, listen:false).borrarScanPorId(scans[i]?.id);
-        },
-        background: Container(
-          color: Colors.red
-        ),
-        child: ListTile(
-          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-          title :  Text(scans[i]!.valor.toString()),
-          subtitle : Text(scans[i]!.id.toString()),
-          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          
-        ),
-      )
-      );
+          return const ScanTiles(tipo:'geo');
   }
 }
